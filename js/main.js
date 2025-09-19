395fee55e2a5df8a0664a1f596967da9fc8d7848
Joseth Stiven Aguilar Enciso <stivenaguilar10288@gmail.com>
2025-09-19 16:00:25 -0500

    Progragamos la funcionalidad de crear una nueva tarjeta usando la manipulación del DOM

--- js/main.js
+++ js/main.js
@@ -1,67 +1,78 @@
-const { createElement } = require("react");
 
-const contenedor =document.getElementById('contenedor-tarjetas');
 
-const form = document.getElementById('form-vehiculo');
+const contenedor = document.getElementById('Contenedor-tarjetas');
+
+const form = document.getElementById('vehiculo-form');
+
+
+const addBtn = document.getElementById('agregar-vehiculo');
 form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
     const valorFoto = document.getElementById('foto-input').value;
-    const valorNombre = document.getElementById('nombre-inpu').value;
+    const valorNombre = document.getElementById('nombre-input').value;
     const valorMarca = document.getElementById('marca-input').value;
     const valorKm = document.getElementById('kilometraje-input').value;
     const valorPrecio = document.getElementById('precio-input').value;
     const valorModelo = document.getElementById('modelo-input').value;
-    if (valorFoto && valorNombre && valorMarca && valorModelo && valorKm && valorPrecio == "") {
+    if (valorFoto == "" || valorNombre == "" || valorMarca == "" || valorModelo == "" || valorKm == "" || valorPrecio == "") {
         alert("Por favor es obligatorio llenar todos los campos que estan en la interfaz")
-    }else{
-
     }
-
-    
-
 });
 
-function createVehiculoCard(){
-    const col = createElement('div');
+function createVehiculoCard(valorFoto, valorNombre, valorMarca, valorKm, valorPrecio, valorModelo) {
+    const col = document.createElement('div');
     col.classList.add('col-md-6', 'item-vehiculo');
 
-    const card = createElement('div');
+    const card = document.createElement('div');
     card.classList.add('card', 'h-100');
 
-    const img = createElement('img');
+    const img = document.createElement('img');
     img.classList.add('card-img-top', 'w-100');
     img.setAttribute('alt', 'foto-vehiculo');
+    img.setAttribute('src', valorFoto);
 
-    const cuerpoTarjeta = createElement('div');
+    const cuerpoTarjeta = document.createElement('div');
     cuerpoTarjeta.classList.add('card-body');
 
-    const h3 = createElement('h3');
+    const h3 = document.createElement('h3');
     h3.classList.add('card-title');
+    h3.textContent = valorNombre;
 
-    const h4 = createElement('h4');
+    const h4 = document.createElement('h4');
     h4.classList.add('card-sibtitle', 'text-muted');
+    h4.textContent = valorMarca;
 
-    const segundoH4 = createElement('h4');
+    const segundoH4 = document.createElement('h4');
     segundoH4.classList.add('card-text');
+    segundoH4.textContent = valorModelo;
 
-    const tercerH4 = createElement('h4');
+
+    const tercerH4 = document.createElement('h4');
     tercerH4.classList.add('card-text');
+    tercerH4.textContent = valorKm;
 
-    const h2 = createElement('h2');
-    h2.classList.add('text-succes');
+    const h2 = document.createElement('h2');
+    h2.classList.add('text-success');
+    h2.textContent = valorPrecio;
 
-    const contenedoresBtn = createElement('div');
+
+    const contenedoresBtn = document.createElement('div');
     contenedoresBtn.classList.add('d-flex', 'justify-content-between', 'mt-3');
 
-    const boton1 = createElement('button');
-    boton1.classList.add('btn', 'btn-succes');
+    const boton1 = document.createElement('button');
+    boton1.classList.add('btn', 'btn-success');
+    boton1.textContent = 'Comprar';
 
-    const boton2 = createElement('button');
+    const boton2 = document.createElement('button');
     boton2.classList.add('btn', 'btn-danger');
+    boton2.textContent = 'Eliminar';
 
-    contenedor.appendChild(col);
     col.appendChild(card);
 
     card.appendChild(img);
+    card.appendChild(cuerpoTarjeta);
+    card.appendChild(contenedoresBtn);
+
 
     cuerpoTarjeta.appendChild(h3);
     cuerpoTarjeta.appendChild(h4);
@@ -71,4 +82,27 @@ function createVehiculoCard(){
 
     contenedoresBtn.appendChild(boton1);
     contenedoresBtn.appendChild(boton2);
+
+    return col;
 };
+
+
+function addVehiculoCard(event) {
+    addBtn.addEventListener('click', () => {
+        const valorFoto = document.getElementById('foto-input').value;
+        const valorNombre = document.getElementById('nombre-input').value;
+        const valorMarca = document.getElementById('marca-input').value;
+        const valorKm = document.getElementById('kilometraje-input').value;
+        const valorPrecio = document.getElementById('precio-input').value;
+        const valorModelo = document.getElementById('modelo-input').value;
+
+        if (valorFoto == "" || valorNombre == "" || valorMarca == "" || valorModelo == "" || valorKm == "" || valorPrecio == "") {
+            alert("Por favor es obligatorio llenar todos los campos que estan en la interfaz")
+        } else {
+            const nuevaTrajeta = createVehiculoCard(valorFoto, valorNombre, valorMarca, valorKm, valorPrecio, valorModelo);
+            contenedor.appendChild(nuevaTrajeta);
+            form.reset();
+        }
+    });
+}
+addVehiculoCard();
